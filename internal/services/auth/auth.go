062c54cf743649809cbcae74d98410783c1dd45d
kirin <rudenokaleksandr21@gmail.com>
2024-11-21 16:48:16 +0000

    update services layer

--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -17,6 +17,7 @@ import (
 type UserStorage interface {
 	SaveUser(ctx context.Context, email string, passHash []byte) (uid int64, err error)
 	User(ctx context.Context, email string) (models.User, error)
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
 }
 
 type AppProvider interface {
@@ -33,6 +34,8 @@ type Auth struct {
 
 var (
 	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrInvalidAppID       = errors.New("invalid app id")
+	ErrUserExists         = errors.New("user already exists")
 )
 
 func New(log *slog.Logger, userSaver UserStorage, userProvider UserStorage, appProvider AppProvider, tokenTTL time.Duration) *Auth {
@@ -64,6 +67,12 @@ func (a *Auth) RegisterNewUser(ctx context.Context, email string, pass string) (
 
 	id, err := a.usrSaver.SaveUser(ctx, email, passHash)
 	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			log.Warn("user already exists", sl.Err(err))
+
+			return 0, fmt.Errorf("%s: %w", op, ErrUserExists)
+		}
+
 		log.Error("failed to save user", sl.Err(err))
 
 		return 0, fmt.Errorf("%s: %w", op, err)
@@ -116,4 +125,29 @@ func (a *Auth) Login(ctx context.Context, email string, password string, appID i
 	}
 
 	return token, nil
+}
+
+func (a *Auth) IsAdmin(cxt context.Context, userID int64) (bool, error) {
+	const op = "Auth.IsAdmin"
+
+	log := a.log.With(
+		slog.String("op", op),
+		slog.Int64("user_id", userID),
+	)
+
+	log.Info("checking if user is admin")
+
+	isAdmin, err := a.usrProvider.IsAdmin(cxt, userID)
+	if err != nil {
+		if errors.Is(err, storage.ErrAppNotFound) {
+			log.Warn("user not found", sl.Err(err))
+
+			return false, fmt.Errorf("%s: %w", op, ErrInvalidAppID)
+		}
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.Info("checking if user is admin", slog.Bool("is_admin", isAdmin))
+
+	return isAdmin, nil
 }
