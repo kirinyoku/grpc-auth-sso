a13867c4bce6b306c8367d7a932c26cbf6a82edb
kirin <rudenokaleksandr21@gmail.com>
2024-11-21 16:44:45 +0000

    update app constructor

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,14 +5,23 @@ import (
 	"time"
 
 	grpcapp "github.com/kirinyoku/grpc-auth-sso/internal/app/grpc"
+	"github.com/kirinyoku/grpc-auth-sso/internal/services/auth"
+	"github.com/kirinyoku/grpc-auth-sso/internal/storage/sqlite"
 )
 
 type App struct {
 	GRPCApp *grpcapp.App
 }
 
-func New(log *slog.Logger, grpcPort int, storagePath string, tokneTTL time.Duration) *App {
-	grpcApp := grpcapp.New(log, grpcPort)
+func New(log *slog.Logger, grpcPort int, storagePath string, tokenTTL time.Duration) *App {
+	storage, err := sqlite.New(storagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	authService := auth.New(log, storage, storage, storage, tokenTTL)
+
+	grpcApp := grpcapp.New(log, authService, grpcPort)
 
 	return &App{
 		GRPCApp: grpcApp,
